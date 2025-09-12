116a57bef2a7bd942788a7dd31acbc3ec01e0362
CeasarCalvo <ceasarcalvomunoz@gmail.com>
2025-09-12 01:48:28 -0600

    feat:Correcton of path duplicate

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ def read_root():
     return {"Hello": getenv("PGADMIN_DEFAULT_EMAIL", "No email configured")}
 
 # Incluir todos los routers
-app.include_router(issue_router, prefix="/issues", tags=["Issues"])
-app.include_router(issue_type_router, prefix="/issue-types", tags=["Issue Types"])
-app.include_router(project_router, prefix="/projects", tags=["Projects"])
-app.include_router(sprint_router, prefix="/sprints", tags=["Sprints"])
-app.include_router(user_project_router, prefix="/user-projects", tags=["User Projects"])
-app.include_router(membership_plan_router, prefix="/membership-plans", tags=["Membership Plans"])
-app.include_router(payment_info_router, prefix="/payment-info", tags=["Payment Info"])
-app.include_router(recover_password_router, prefix="/recover-passwords", tags=["Recover Password"])
-app.include_router(user_router, prefix="/users", tags=["Users"])
+app.include_router(issue_router, tags=["Issues"])
+app.include_router(issue_type_router, tags=["Issue Types"])
+app.include_router(project_router, tags=["Projects"])
+app.include_router(sprint_router, tags=["Sprints"])
+app.include_router(user_project_router, tags=["User Projects"])
+app.include_router(membership_plan_router, tags=["Membership Plans"])
+app.include_router(payment_info_router, tags=["Payment Info"])
+app.include_router(recover_password_router, tags=["Recover Password"])
+app.include_router(user_router,  tags=["Users"])
